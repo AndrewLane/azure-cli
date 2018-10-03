2a2e3420696f11de28a02fe4b86fae45b79c1785
Troy Dai <troy.dai@outlook.com>
2018-10-03 13:54:57 -0700

    [skip ci] update

--- scripts/release/homebrew/pipeline.sh
+++ scripts/release/homebrew/pipeline.sh
@@ -5,23 +5,8 @@ root=$(cd $(dirname $0); pwd)
 set -e
 
 CLI_VERSION=`cat $SYSTEM_ARTIFACTSDIRECTORY/metadata/version`
-
-export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string -g azure-cli-prod -n azurecliprod -otsv)
+HOMEBREW_UPSTREAM_URL=`cat $BUILD_STAGINGDIRECTORY/github/upstream_url`
 TAR_NAME=azure-cli-$CLI_VERSION.tar.gz
-
-if [ ! -d ./archive/pypi ]; then
-    az storage blob download-batch -s $RELEASE_CONTAINER_NAME -d ./artifacts --pattern 'pypi/*'
-fi
-
-mkdir -p $BUILD_STAGINGDIRECTORY/github/ >/dev/null 2>&1
-mkdir -p $BUILD_STAGINGDIRECTORY/homebrew/ >/dev/null 2>&1
-curl -sL https://github.com/Azure/azure-cli/archive/$TAR_NAME -o $BUILD_STAGINGDIRECTORY/github/$TAR_NAME
-
-echo "Upload Azure CLI Source Package"
-az storage blob upload -c releases -n $TAR_NAME -f $BUILD_STAGINGDIRECTORY/github/$TAR_NAME --no-progress -ojson
-
-HOMEBREW_UPSTREAM_URL=$(az storage blob url -c releases -n $TAR_NAME -otsv)
-curl -sfS -I $HOMEBREW_UPSTREAM_URL >/dev/null
 
 docker_files=$(cd $root/docker; pwd)
 pypi_files=$(cd $SYSTEM_ARTIFACTSDIRECTORY/pypi; pwd)
@@ -38,5 +23,5 @@ docker run -v $docker_files:/mnt/scripts \
            python:3.6 \
            /mnt/scripts/run.sh
 
-docker cp $container_name:azure-cli.rb ./artifacts/homebrew/azure-cli.rb
+docker cp $container_name:azure-cli.rb $BUILD_STAGINGDIRECTORY/azure-cli.rb
 docker rm $container_name
